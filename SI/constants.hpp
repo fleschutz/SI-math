a321be8301b3b2ef7d8d278c8add839b8f5e7891
Markus Fleschutz <markus.fleschutz@gmail.com>
2025-12-07 14:17:10 +0100

    Added some constants and distances

--- SI/constants.hpp
+++ SI/constants.hpp
@@ -5,13 +5,17 @@
 namespace si
 {
 	// Physical constants:
-	inline static const auto gravity = meters_per_second2(9.80665); // standard gravitational acceleration (G) for the surface of the Earth, defined 1901 in the third General Conference on Weights and Measures.
+	const auto gravity_of_Earth = meters_per_second2(9.80665); // standard gravitational acceleration (G) for the surface of the Earth, defined 1901 in the third General Conference on Weights and Measures.
+	const auto gravity_of_Mars = meters_per_second2(3.728);
+	const auto gravity_of_Mercury = meters_per_second2(3.7); // surface gravity
+	const auto gravity_of_Moon = meters_per_second2(1.628);
 									
-	inline static const auto speed_of_light = meters_per_second(299792458); // speed of light in vacuum (c)
+	const auto speed_of_light = meters_per_second(299792458); // speed of light in vacuum (c)
 
+	const auto speed_of_sound = meters_per_second(343); // speed of sound in air at 20 °C (68 °F)
 
 	// Mathematical constants:
-	inline static const auto pi = degree(180);
-	inline static const auto pi_2 = degree(90);
-	inline static const auto tau = degree(360);
+	const auto pi = degree(180);
+	const auto half_pi = degree(90);
+	const auto tau = degree(360);
 }	
--- SI/distances.hpp
+++ SI/distances.hpp
@@ -4,7 +4,9 @@
 
 namespace si
 {
-	// sorted alphatically:
+	// source: distancecalculator.net (sorted alphabetically)
+	const length Boston_to_NewYorkCity = kilometer(306);
 	const length Berlin_to_Paris = kilometer(878.080);
+	const length NewYorkCity_to_Boston = kilometer(306);
 	const length Paris_to_Berlin = kilometer(878.080);
 }
