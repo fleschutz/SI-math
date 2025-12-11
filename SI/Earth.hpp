da0be8612a6b1db9999a19f4b10e2cb2e346a5b4
Markus Fleschutz <markus.fleschutz@gmail.com>
2025-12-11 18:39:19 +0100

    Renamed to buildings.hpp and mountains.hpp

--- SI/Earth.hpp
+++ SI/Earth.hpp
@@ -9,12 +9,15 @@ namespace si
 		const auto gravity = meters_per_second2(9.80665); // standard gravitational acceleration (G) for the surface of the Earth, defined 1901 in the third General Conference on Weights and Measures.
 
 		const auto equatorial_radius = si::kilometer(6378.1370); // denoted 'a'
+		const auto equatorial_diameter = 2.0 * equatorial_radius;
+		const auto equatorial_circumference = si::kilometer(40075.0167); 
+
 		const auto polar_radius = si::kilometer(6356.7523);      // denoted 'b'
+		const auto polar_diameter = 2.0 * polar_radius;
+		const auto polar_circumference = si::kilometer(40007.8629173);
+
 		const auto radius = si::kilometer(6371);                 // (2*a+b) / 3
 		const auto diameter = 2.0 * radius;
-
-		const auto equatorial_circumference = si::kilometer(40075.0167); 
-		const auto polar_circumference = si::kilometer(40007.8629173);
 
 		const auto Great_Wall_length = kilometer(6430); // (approximately)
 	}
